9c55a2ff6fdd3a001c1693a000d86efbc0196817
Roman 2517 <benybeny2517@gmail.com>
2021-06-18 20:40:48 +0300

    first commits variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let D = (b**2) - 4*a*c;
+  let x = (-b) / 2*a;
+  let x1 = (-b + Math.sqrt(D) )/(2*a);
+  let x2 = (-b - Math.sqrt(D) )/(2*a);
+
+    if(D === 0){
+      arr[0] = x;
+    } else if (D > 0){
+      arr[0] = x1;
+      arr[1] = x2;
+    }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
